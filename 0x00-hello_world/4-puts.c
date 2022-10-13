115847c104bd4b0b8a09d6e0248699082c02c136
Sphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-14 01:12:44 +0200

    Commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,1 @@
 #include <stdio.h>
-
-/**
- * main prints a phrase
- * Return: Always 0 (Success)
-*/
-int main(void)
-{
-    puts("\"Programming is like building a multilingual puzzle");
-    return(0);
-}
