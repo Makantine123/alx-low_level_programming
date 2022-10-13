f8bb6c4b0b827812b2a1aefe7f95decc3a5889a1
Sphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-14 00:49:21 +0200

    Commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main(void)
 {
-    char str1 = "Programming is like building a multilingual puzzle\n";
+    char str1 = "\"Programming is like building a multilingual puzzle\n";
     puts(str1);
     return(0);
 }
