a89f4419e551ab5fa2b699061f9aa1f4ff47b6a3
Sphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-14 20:55:24 +0200

    Commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char Num1;
+	char Num2;
+	char set[];
+	int i;
+	i = 0;
+
+	for (Num1 = 0; Num1 < 10; Num1++)
+	{
+		for (Num2 = 0; Num2 <10; Num2++)
+		{
+			set[i] = Num1 + Num2
+			printf("%d",set[i])
+		}
+	}
+	return (0);
+}
