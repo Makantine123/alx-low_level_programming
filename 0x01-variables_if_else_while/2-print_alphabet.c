df11222332f30030f5f138f1748a16f1199e57d1
Sphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-14 18:02:35 +0200

    Commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet;
+		
+	for (c = 'a'; c <= 'z'; c++)
+		putchar("%d\n",alphabet);
+	return (0);
+}
