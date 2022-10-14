00f7bedb3cd81a0e23e7e2da63c1e1ad200fd889
Sphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-14 02:33:56 +0200

    Commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
  */
 int main(void)
 {
-	fwrite(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	fwrite(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
