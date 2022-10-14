6be735d48e49f3ff414284cdf36cfb18e6cd96c7
Sphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-14 17:41:50 +0200

    Commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ int main(void)
 	if (n % 10 > 5)
 	{
 		printf("Last digit of %d is greater than 5\n", n);
-	} else if (n % 10 = 0)
+	} else if (n % 10 == 0)
 	{
 		printf("Last digit of %d is 0\n", n);
 	} else if (n % 10 < 6  && n % 10 != 0)
