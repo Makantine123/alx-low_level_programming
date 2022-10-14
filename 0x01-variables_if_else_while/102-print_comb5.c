c06c738d7ce9ff2663856e2dd76f66910ea088f2
Sphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-14 22:43:01 +0200

    Commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -20,7 +20,7 @@ int main(void)
 				putchar(' ');
 				putchar((num2 / 10) + '0');
 				putchar((num2 % 10) + '0');
-				if (i != 98 || j != 99)
+				if (num1 != 98 || num2 != 99)
 				{
 					putchar(',');
 					putchar(' ');
