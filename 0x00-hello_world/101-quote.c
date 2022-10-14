7b5d842a2d2113ba53722a13aaa681414611920a
Sphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-14 02:46:38 +0200

    Commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -8,7 +8,6 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-			sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
