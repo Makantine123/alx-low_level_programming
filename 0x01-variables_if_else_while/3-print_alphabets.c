0624c57deae88347ecd4c384a63dc867dc2e9dd8
Sphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-14 18:28:55 +0200

    Commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphatbet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	for  (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
