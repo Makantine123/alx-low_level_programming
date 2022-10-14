1b42e7cbe1823a18c7fc536b9e9d7b24acd1ec4f
Sphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-14 16:29:17 +0200

    Commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,12 +16,10 @@ int main(void)
 	if (n < 0)
 	{
 		printf("%d is negative\n",n);
-	}
-	if (n > 0)
+	} if (n > 0)
 	{
 		printf("%d is positive\n",n);
-	}
-	if (n == 0)
+	} if (n == 0)
 	{
 		printf("%d is zero\n",n);
 	}
