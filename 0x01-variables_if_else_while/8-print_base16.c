c82746338c11b8f10495ab94fffe86ba2d7ad49a
Sphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-14 19:51:45 +0200

    Commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always return 0 (Success)
+ */
+int main(void)
+{
+	char base16;
+
+	for (base16 = 0; base16 < 10; base16++)
+	{
+		putchar(base16);
+	}
+	for (base16 = 'a'; base16 <= 'f'; base16++)
+	{
+		puchar(base16);
+	}
+	putchar('\n');
+	return (0);
+}
