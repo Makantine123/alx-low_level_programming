60ff4849aa2d4ad4b71a99c1d0efe1e28fd5e512
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-18 22:12:10 +0200

    Commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - Checks if n is greater than 0, equal to zero, less than zero
+ * Return: Returns the absolute value of a number
+ * @int: Interger
+ */
+int _abs(int)
+{
+if (int < 0)
+{
+_putchar(-int);
+}
+else
+{
+_putchar(int);
+}
+}
