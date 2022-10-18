2d43ef9a1a18ed6a68504bd448e5cdfc3cd6ad61
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-18 21:55:32 +0200

    Commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - Checks if n is greater than 0, equal to zero, less than zero
+ * Return: Returns  1, 0, -1 and prints +, 0, -
+ * @n: Is a number to be tested
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar("+");
+return (1);
+}
+else if (n == 0)
+{
+_putchar("0");
+return (0);
+}
+else if (n < 0)
+{
+_putchar("-");
+return (-1);
+}
+}
