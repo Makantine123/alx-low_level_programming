a565ed85e91aab6975e7a2fbdf0e5dbb6357fc07
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-18 21:02:05 +0200

    Commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
  * main - Prints letter in small case and then new line
- * Refers to main header for function prototype
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
