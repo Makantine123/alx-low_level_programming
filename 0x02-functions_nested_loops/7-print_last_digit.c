6cc5c964bd91c2291fc62d5fb1f00cc012e60a20
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-18 22:30:25 +0200

    Commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit
+ * Return: Returns the absolute value of a number
+ * @i: Interger
+ */
+int print_last_digit(int i)
+{
+int j = i % 10;
+return (j);
+}
