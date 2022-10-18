a3c00e4a091623e06101c1c3d51542b2b0f8b60a
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-18 21:36:19 +0200

    Commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints letter in small case 10x and then new line
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (0);
+}
+else
+{
+return (1);
+}
+}
