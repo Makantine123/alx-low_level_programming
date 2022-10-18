4967cc73a0d13faf4d925a9d0060c77f8e2f0d03
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-18 20:20:06 +0200

    Commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,13 +4,8 @@
  * 
  * Return: Always 0 (Success)
  */
-void print_alphabet(void)
+int main(void)
 {
-    char c;
-
-    for ( c = 'a'; c <= 'z'; c++ )
-    {
-        _putchar(c);
-    }
-    _putchar('\n');
+    print_alphabet();
+    return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,3 +10,14 @@ int _putchar(char c)
 {
     return(write(1, &c , 1));
 }
+
+void print_alphabet(void)
+{
+    char c;
+
+    for ( c = 'a'; c <= 'z'; c++ )
+    {
+        _putchar(c);
+    }
+    _putchar('\n');
+}
