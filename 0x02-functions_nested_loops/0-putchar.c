5420cef6af0b652e0065b3c87fc3b985bf7a23b1
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-18 18:08:29 +0200

    Commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - Print _putchar, followed by new line
- * 
+ * main - Entry point
  * Return: Always 0 (Success)
 */
 int main(void)
 {
-    printf("_putchar\n");
-    return (0);
+	printf("_putchar\n");
+	return (0);
 }
