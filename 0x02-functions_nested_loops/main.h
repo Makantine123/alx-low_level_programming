dcb40e53a74ba117426f01360cdae087fea90482
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-18 20:23:00 +0200

    Commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,15 +1,4 @@
 #include "unistd.h"
-/**
- * _putchar - writes the character c to stdout
- * @c: the character to print
- * 
- * Return: On succes 1
- * On error: -1 is returned, and errno is set appropiately
- */
-int _putchar(char c)
-{
-    return(write(1, &c , 1));
-}
 
 void print_alphabet(void)
 {
