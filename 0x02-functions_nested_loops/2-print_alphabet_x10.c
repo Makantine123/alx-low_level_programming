590e7a70694c4377d824d66f64a8400b29513185
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-18 21:26:59 +0200

    Commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints letter in small case 10x and then new line
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+int i;
+int j;
+char c;
+
+for (i = 0; i < 10; i++)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+}
