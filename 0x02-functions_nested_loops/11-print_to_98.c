3b340bb071fce5ca3745fa9fe8f4edbf14969e00
Siphelele makhathini <siphelele.mlungisi@gmail.com>
2022-10-19 09:27:30 +0200

    Commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,24 +7,24 @@
  */
 void print_to_98(int n)
 {
-    if (n < 98)
-    {
-        for (n = n; n < 98; n++)
-        {
-            printf("%d, ", n);
-        }
-        printf("98\n");
-    }
-    else if (n > 98)
-    {
-        for (n = n; n > 98; n--)
-        {
-            printf("%d, ",n);
-        }
-        printf("98\n");
-    }
-    else
-    {
-        printf("98\n");
-    }
+if (n < 98)
+{
+for (n = n; n < 98; n++)
+{
+printf("%d, ", n);
+}
+printf("98\n");
+}
+else if (n > 98)
+{
+for (n = n; n > 98; n--)
+{
+printf("%d, ",n);
+}
+printf("98\n");
+}
+else
+{
+printf("98\n");
+}
 }
