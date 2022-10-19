64c16ac1f7d6b66099193103c9e3246efeb82b7f
Siphelele makhathini <siphelele.mlungisi@gmail.com>
2022-10-19 08:24:09 +0200

    Commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - Prints letter in small case and then new line
+ * Return: Returns the result of 2 intergers
+ */
+int add(int n, int k)
+{
+int res;
+res = n + k;
+return (res);
+}
