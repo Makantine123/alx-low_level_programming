546ccc30382cb026c263e74663f266929636d4c1
Siphelele makhathini <siphelele.mlungisi@gmail.com>
2022-10-19 14:23:35 +0200

    Commit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,9 +6,9 @@
 int main(void)
 {
 unsigned int i;
-unsigned long _1stNum;
-unsigned long _2ndNum;
-unsigned long _nxtNum;
+unsigned int _1stNum;
+unsigned int _2ndNum;
+unsigned int _nxtNum;
 _1stNum = 0;
 _2ndNum = 1;
 for (i = 0; i < 98; i++)
@@ -19,7 +19,7 @@ _2ndNum = _nxtNum;
 printf("%u", _nxtNum);
 if (i < 96)
 {
-printf(", ");
+    printf("%u", _nxtNum);
 }
 }
 printf("\n");
