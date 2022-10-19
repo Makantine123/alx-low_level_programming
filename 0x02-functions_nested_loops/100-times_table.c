fad3b0602ddda7d95e61d933b4723ea0f7cdec5b
Siphelele makhathini <siphelele.mlungisi@gmail.com>
2022-10-19 12:00:35 +0200

    Commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -27,7 +27,7 @@ if (res >= 100)
 _putchar(',');
 _putchar(' ');
 _putchar((res / 100) + '0');
-_putchar((res / 10) + '0');
+_putchar((res / 10) % 10 + '0');
 _putchar((res % 10) + '0');
 }
 if (res >= 10 && res < 100)
