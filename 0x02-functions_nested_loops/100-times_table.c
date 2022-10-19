cdcf7540ebdf5138b6f301ed32fd25f0b191b4b4
Siphelele makhathini <siphelele.mlungisi@gmail.com>
2022-10-19 11:26:51 +0200

    Commit

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints letter in small case and then new line
+ * Return: Always 0 (Success)
+ * @n: Integer to pass to create times table
+ */
+void print_times_table(int n)
+{
+int i;
+int j;
+int res;
+
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+res = i * j;
+if (j != 0)
+{
+_putchar(',');
+_putchar(' ');
+}
+if (res >= 10)
+{
+_putchar((res / 10) + '0');
+_putchar((res % 10) + '0');
+}
+else if (res < 10 && j != 0)
+{
+_putchar(' ');
+_putchar((res % 10) + '0');
+}
+else
+{
+_putchar((res % 10) + '0');
+}
+}
+_putchar('\n');
+}
+}
