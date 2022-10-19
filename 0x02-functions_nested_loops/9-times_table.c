b982a7921e16c55e84dfec3f72130ea2482c6699
Siphelele makhathini <siphelele.mlungisi@gmail.com>
2022-10-19 08:09:30 +0200

    Commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,28 +12,27 @@ for (i = 0; i < 10; i++)
 {
     for (j = 0; j < 10; j++)
     {
-        res = i*j;
-
-        if (j != 0)
-        {
-            _putchar(',');
-            _putchar(' ');
-        }
-        if (res >= 10)
-        {
-            _putchar((res / 10) + '0');
-            _putchar((res % 10) + '0');
-        }
-        else if (res < 10 && j != 0)
-        {
-            _putchar(' ');
-            _putchar((res % 10) + '0');
-        }
-        else
-        {
-            _putchar((res % 10) + '0');
-        }
-    }
-    _putchar('\n');
+res = i*j;
+if (j != 0)
+{
+_putchar(',');
+_putchar(' ');
+}
+if (res >= 10)
+{
+_putchar((res / 10) + '0');
+_putchar((res % 10) + '0');
+}
+else if (res < 10 && j != 0)
+{
+_putchar(' ');
+_putchar((res % 10) + '0');
+}
+else
+{
+_putchar((res % 10) + '0');
+}
+}
+_putchar('\n');
 }
 }
