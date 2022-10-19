fbcc90aa27640938c368e2d7f661f0108c5f7065
Siphelele makhathini <siphelele.mlungisi@gmail.com>
2022-10-19 13:35:50 +0200

    Commit

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -5,24 +5,23 @@
  */
 int main(void)
 {
-    int i;
-    unsigned long _1stNum;
-    unsigned long _2ndNum;
-    unsigned long _nxtNum;
-
-    _1stNum = 0;
-    _2ndNum = 1;
-
-    for (i = 1; i <= 50; i++)
-    {
-        _nxtNum = _1stNum + _2ndNum;
-        _1stNum = _2ndNum;
-        _2ndNum = _nxtNum;
-        printf("%ld", _nxtNum);
-        if (i < 50)
-        {
-        printf(", ");
-        }
-    }
-    return (0);
+int i;
+unsigned long _1stNum;
+unsigned long _2ndNum;
+unsigned long _nxtNum;
+_1stNum = 0;
+_2ndNum = 1;
+for (i = 1; i <= 50; i++)
+{
+_nxtNum = _1stNum + _2ndNum;
+_1stNum = _2ndNum;
+_2ndNum = _nxtNum;
+printf("%ld", _nxtNum);
+if (i < 50)
+{
+printf(", ");
+}
+}
+printf("\n");
+return (0);
 }
