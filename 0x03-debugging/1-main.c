7962f6207c69ef2d5d502edafe222ad22f7e025f
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-20 16:47:59 +0200

    Commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,17 +7,18 @@
 
 int main(void)
 {
-	int i;
+int i;
 
-	printf("Infinite loop incoming :(\n");
+printf("Infinite loop incoming :(\n");
 
-	i = 0;
+i = 0;
 
-	{
-		putchar(i);
-	}
 
-	printf("Infinite loop avoided! \\o/\n");
+{
+putchar(i);
+}
 
-	return (0);
+printf("Infinite loop avoided! \\o/\n");
+
+return (0);
 }
