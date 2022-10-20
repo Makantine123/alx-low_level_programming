d92703e186c74870713b8ab5f5945e981a9dbd3b
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-20 16:49:03 +0200

    Commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,24 +1,17 @@
 #include <stdio.h>
-
 /**
  * main - cause as infinite loop
  * Return: 0
  */
-
 int main(void)
 {
 int i;
 
 printf("Infinite loop incoming :(\n");
-
 i = 0;
-
-
 {
 putchar(i);
 }
-
 printf("Infinite loop avoided! \\o/\n");
-
 return (0);
 }
