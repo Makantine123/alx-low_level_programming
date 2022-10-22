d403a5dc9226a534fec256bd80134e4af1699da5
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-22 17:06:21 +0200

    Commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * main - Print Fizz-Buzz
+ * Return: Always 0
+ */
+void main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100, i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printif("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (i < 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+}
