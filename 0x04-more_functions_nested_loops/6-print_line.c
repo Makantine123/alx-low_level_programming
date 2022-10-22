6e20a0600bbd5f7fa465f41d3706e855706c350e
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-22 15:34:29 +0200

    Commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_line - Drawas straight line in terminal
+ * Return: Always zero
+ * @n: Number of times _ should print
+ */
+void print_line(int n)
+{
+	int t;
+	if (n > 0)
+	{
+		for (t = 0; t <= n; t++)
+		{
+		_putchar('_');
+		}
+	}
+	else
+	{
+	_puchar('\n');
+	}
+}
