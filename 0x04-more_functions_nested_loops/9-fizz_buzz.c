a22a47530c4e89fe2d6199c1d1cc34e4d0c4e822
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-22 17:10:55 +0200

    Commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,15 +1,16 @@
-#include "stdio.h"
+#include <stdio.h>
+#include "main.h"
 /**
  * main - Print Fizz-Buzz
  * Return: Always 0
  */
-void main(void)
+int main(void)
 {
 	int i;
 
 	for (i = 1; i <= 100, i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if (i % 15 == 0)
 		{
 			printf("FizzBuzz");
 		}
@@ -23,7 +24,7 @@ void main(void)
 		}
 		else
 		{
-			printf("%d", i);
+			printf("%i", i);
 		}
 		if (i < 100)
 		{
@@ -31,4 +32,5 @@ void main(void)
 		}
 	}
 	printf("\n");
+	return (0);
 }
