8fadb913076fa7264409ca7724192c74a2d4ce38
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-22 14:57:08 +0200

    Commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,18 +1,20 @@
 #include "main.h"
+
 /**
- * print_most_numbers - prints numbers 0 to 9, does not print 2 and 4
- * Return: Always 0
+ * print_most_numbers - prints
+ *
+ * Return: void
  */
+
+
 void print_most_numbers(void)
 {
-	int i;
+	char c;
 
-	for (i = '0'; i < '10'; i++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		if (!(i == '2' || i == '4'))
-		{
-			_putchar(i + '0');
-		}
+		if (!(c == '2' || c == '4'))
+			_putchar(c);
 	}
 	_putchar('\n');
 }
