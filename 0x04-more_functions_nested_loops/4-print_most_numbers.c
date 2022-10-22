0edad5181aaf99c143e29329b29aea2725765a5f
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-22 14:52:50 +0200

    Commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,13 +5,13 @@
  */
 void print_most_numbers(void)
 {
-	char i;
+	int i;
 
 	for (i = '0'; i < '10'; i++)
 	{
 		if (!(i == '2' || i == '4'))
 		{
-			_putchar(i);
+			_putchar(i + '0');
 		}
 	}
 	_putchar('\n');
