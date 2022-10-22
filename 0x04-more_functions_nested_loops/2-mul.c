44ec6b6ddd86a2ec80ca7d694758d3c93739a8bb
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-22 14:19:31 +0200

    Commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - Multiplies 2 integers
+ * @a: First integer a
+ * @b: Second integer b
+ * Return: Multiplies 2 numbers
+ */
+int mul(int a, int b)
+{
+	unsigned long mult;
+
+	mult = a * b;
+
+	return (mult);
+}
