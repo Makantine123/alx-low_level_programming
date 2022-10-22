d77d4dfcccac246583ff8309971b3ddab1c94e68
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-22 15:57:24 +0200

    Commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_diagonal - Draws a line in terminal
+ * Return: Lines
+ * @n: Number of '_' to print
+ */
+void print_diagonal(int n)
+{
+	t = 0;
+	for (t =0; t < n; t++)
+	{
+		_putchar('\');
+	}
+	_putchar('\n');
+}
