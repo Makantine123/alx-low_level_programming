ded33092567963737e3d229d6c115f9fc1a7a843
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-22 15:22:39 +0200

    Commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,19 +5,16 @@
  */
 void more_numbers(void)
 {
-	int i,j;
-
-	for (j = 0; j < 10; j++)
+	
+int i, j;
+	for (i = 1; i <= 10; i++)
 	{
-
-		for (i = 0; i < 15; i++)
+		for (j = 0; j < 15; j++)
 		{
-			if (i > 9)
-			{	
-				_putchar('1')
-			}
-			_putchar(i % 10 + '0');
+			if (j >= 10)
+			_putchar('1');
+			_putchar(j % 10 + '0');
 		}
-		_putchar('\n');
-	}
+			_putchar('\n');
+		}
 }
