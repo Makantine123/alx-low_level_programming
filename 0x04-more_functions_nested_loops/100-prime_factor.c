44569263dfca3c7fb86f84faf0f189dc62c4f3d4
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-23 20:39:18 +0200

    Commit

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,36 +1,38 @@
 #include <stdio.h>
-
-int main()
+/**
+ * main - find largest prime factor
+ * Return: Always 0
+*/
+int main(void)
 {
-    int i, j, num, isPrime, lrg;
+int i, j, num, isPrime, lrg;
 
     /* Input a number from user */
-    num = 1231952;
+num = 1231952;
 
     /* Find all Prime factors */
-    for(i=2; i<=num; i++)
-    {
-        /* Check 'i' for factor of num */
-        if(num%i==0)
-        {
-            /* Check 'i' for Prime */
-            isPrime = 1;
-            for(j=2; j<=i/2; j++)
-            {
-                if(i%j==0)
-                {
-                    isPrime = 0;
-                    break;
-                }
-            }
-
-            /* If 'i' is Prime number and factor of num */
-            if(isPrime==1)
-            {
-                lrg = i;
-            }
-        }
-    }
-	printf("%d\n",lrg);
-    return (0);
+for (i = 2; i <= num; i++)
+{
+ /* Check 'i' for factor of num */
+if (num % i == 0)
+{
+/* Check 'i' for Prime */
+isPrime = 1;
+for (j = 2; j <= i / 2; j++)
+{
+if (i % j == 0)
+{
+isPrime = 0;
+break;
+}
+}
+/* If 'i' is Prime number and factor of num */
+if (isPrime == 1)
+{
+lrg = i;
+}
+}
+}
+printf("%d\n", lrg);
+return (0);
 }
