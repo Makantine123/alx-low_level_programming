64208cf28e880c5415de41ea46aa2c23f3fcf149
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-24 23:11:17 +0200

    Commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * reset_to_98 - takes pointer and updates the value it points to 98
- * @*n: Pointer parameter
+ * @n: Pointer parameter
  * Return: Always 0
  */
 void reset_to_98(int *n)
