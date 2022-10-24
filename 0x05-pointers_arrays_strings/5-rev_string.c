097275f064c42d1f29a021e050ad65cbdaba4351
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-25 00:25:15 +0200

    Commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rev_string - Reverses a string
+ * @s: String to be reversed
+ * Return: Always void
+ */
+void rev_string(char *s)
+{
+	int i;
+	int j;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	j = i - 1;
+	while (s[j] != '\0')
+	{
+		_putchar(s[j]);
+		j--;
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -11,3 +11,5 @@ int _putchar(char c);
 void _puts(char *str);
 
 void print_rev(char *s);
+
+void rev_string(char *s);
