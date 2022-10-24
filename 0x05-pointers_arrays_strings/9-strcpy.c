9ef2309b9f8a167006ab02bf5534eb442b0f135f
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-25 01:38:28 +0200

    Commit

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *_strcpy - Copy string pointed by src, include \0 to pointer dest
+ * Return: the pointer dest
+ * @dest: Pointer to copy to
+ * @src: Pointer of string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+
+	while (src[i] != '\n')
+	{
+		*(src + i) = *(dest + i);
+		i ++;
+	}
+	return (dest);
+}
