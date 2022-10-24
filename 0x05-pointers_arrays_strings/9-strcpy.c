5363b7ad05915b7ced71ec2b4b9e4b51ca6856f6
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-25 01:47:43 +0200

    Commit

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,10 +11,11 @@ char *_strcpy(char *dest, char *src)
 
 	i = 0;
 
-	while (src[i] <= '\0')
+	while (src[i] != '\0')
 	{
 		*(dest + i) = src[i];
 		i++;
 	}
+	*(dest + i) = '\0'
 	return (dest);
 }
