10ca7b86b06e4e292b44c1033c3ddddcb1d4efab
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-24 23:18:26 +0200

    Commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * swap_int - Functions swaps the value of 2 intergers
+ * @a: Integer a
+ * @b: Integer b
+ * Return: Nothing
+ */
+void swap_int(int *a, int *b)
+{
+	*a = b;
+	*b = a;
+}
