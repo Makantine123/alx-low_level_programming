a126ddd152c3cb3f289244cbad98ccf2847c4ce5
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-25 01:10:56 +0200

    Commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts_half - Print half the string
+ * @str: String to be processed
+ * Return: Always void
+ */
+void puts_half(char *str)
+{
+	int i;
+	int j;
+	int count;
+
+	i = 0;
+	
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	count = i - 1;
+	for (j = count / 2; str[j] != '\0'; j++)
+	{
+		_putchar(str[j]);
+	}
+	_putchar(str[j]);
+}
