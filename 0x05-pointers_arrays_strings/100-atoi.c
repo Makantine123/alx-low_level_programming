5dad732e2045883287245eb726a7c5735e30d122
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-25 02:23:08 +0200

    Commit

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * _atoi - Converts a string to an integer
+ * @s: String to be converted to integer
+ * Return: 0 if no iterger in string
+ */
+int _atoi(char *s)
+{
+	int sign;
+	int number;
+	int i;
+
+	sign = 1;
+	number =  0;
+	i = 0;
+
+	if (s[i] == '-')
+	{
+		sign = -1;
+		i = 1;
+	}
+
+	while (s[i] != '\0')
+	{
+		if(s[i] >= '0' && s[i] <= '9')
+		{
+			number = (number * 10) + s[i] - '0';
+		}
+		else
+		{
+			break;
+		}
+		i++;
+	}
+	number = number * sign;
+	if (number != '\0')
+	{
+		return (number);
+	}
+	else
+		return (0);
+}
+
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -22,3 +22,5 @@ void print_array(int *a, int n);
 
 char *_strcpy(char *dest, char *src);
 
+int _atoi(char *s);
+
