47fe641aabf1a239965463a547c5b206c3683711
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-29 01:02:30 +0200

    Commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,17 +12,16 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-int i;
+	int i;
 
-i = 0;
-for ( ; i < n && src[i] != '\0'; ++i)
-{
-dest[i] = src[i];
-}
-while (i < n)
-{
-dest[i] = '\0';
-++i;
-}
-return (dest);
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	while (i < n)
+	{
+		dest[i] = '\n';
+		i++;
+	}
+	return (dest);
 }
