275d744af7aeecd434853a2ec1f8df9ead34f9f7
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-28 03:31:55 +0200

    Commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,6 +7,18 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	dest = *dest + *src;
-	return dest;
+	int i, len;
+
+	len = 0;
+
+	while (dest[len] != '\0')
+	{
+		++len;
+	}
+	for (i = 0; src[i] != '\0'; ++i, ++len)
+	{
+		dest[len] = src[i];
+	}
+	dest[len] = '\0'
+	return (dest);
 }
