f47fe0745f07fd314e33f34c4cc5d574a3b6cfcc
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-31 14:56:57 +0200

    Commit

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,5 +9,27 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
+	int i, j, founPref;
 
+	foundPref = 0;
+
+	for (i = 0; *(s + i) >= '\0'; i++)
+	{
+		for (j = 0; *(accept + 0) >= '\0'; j++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				foundPref = 1;
+				if (foundPref == 1)
+				{
+					break;
+				}
+			}
+		}
+		if (foundPref == 1)
+		{
+			break;
+		}
+	}
+	return (i);
 }
