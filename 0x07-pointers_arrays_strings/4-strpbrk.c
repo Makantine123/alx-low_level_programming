647ed8673547ae727571df94cdc1e8736d9b9a76
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-31 16:37:34 +0200

    Commit

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Searches a string (s) for any of a set of bytes
+ * @s: String s
+ * @accept: Bytes to find
+ * Return: Pointer to byte in s that matches bytes in accept or NULL
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, k;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		for (k = 0; *(accept + j) != '\0'; k++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				break;
+			}
+		}
+		if (!(*(s + i)))
+		{
+			break;
+		}
+	}
+	return (*(s + i));
+}
