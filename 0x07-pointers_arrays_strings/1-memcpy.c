cfd8aef479c952ebf3610a493dfa35bc98036adb
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-31 13:40:20 +0200

    Commit

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,7 +11,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int i;
 	/* unsigned int j; */
 
-	for (i = 0; i < n && *(src + i) != '\0'; i++)
+	for (i = 0; i < n && *(dest + i) != '\0'; i++)
 	{
 		*(dest + i) = *(src + i);
 	}
