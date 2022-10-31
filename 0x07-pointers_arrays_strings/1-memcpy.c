32d7cda6718e101f917a2b46b02884d3437cdb58
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-10-31 14:23:08 +0200

    Commit

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,7 +9,6 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-	/* unsigned int j; */
 
 	for (i = 0; i < n; i++)
 	{
---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strspn - Gets the length of a prefix substring
+ * @s: Initial String
+ * @accept:Bytes
+ * Return: Integer bytes
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+
+}
