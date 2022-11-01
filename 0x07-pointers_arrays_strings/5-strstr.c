0252f735b2d9833d2cf263c22d5b1ff6dc909036
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-11-01 15:01:04 +0200

    Commit

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -19,13 +19,8 @@ char *_strstr(char *haystack, char *needle)
 	{
 		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if (needle[j] == haystack[j + i])
+			if (needle[j] != haystack[j + i])
 			{
-				found = true;
-			}
-			else
-			{
-				found = false;
 				break;
 			}
 		}
