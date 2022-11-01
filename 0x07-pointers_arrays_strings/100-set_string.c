e6f9f1013c3d88ffeabc371b4a68b5f321aacd2e
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-11-01 15:09:07 +0200

    Commit

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - Function the sets the value of a pointer to char
+ * @s: Pointer to pointer
+ * @to: Pointer
+ * Return: Nothing, sets value to pointer
+ */
+void set_string(char **s, char *to)
+{
+	int i, j;
+
+}
