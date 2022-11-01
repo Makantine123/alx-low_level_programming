e47b112a8a0f58ddf4d052185146cb25a1239e3d
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-11-01 20:00:41 +0200

    Commit

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * infinite_add - Function adds two numbers
+ * @n1: Number one
+ * @n2: Number two
+ * @r: Buffer that will store the result
+ * @size_r: Buffer size
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+}
