e82c8f8aef6fd6039c8586d35daf20408f664c27
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-11-02 16:00:19 +0200

    Commit

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * factorial - Function finds the factorial of given number
+ * @n: Integer number to find factorial
+ * Return: 1 if n < 0 or factorial
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
+
