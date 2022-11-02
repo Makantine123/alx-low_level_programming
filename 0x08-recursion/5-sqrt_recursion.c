2549f65e6db82737546da1731393206a94cac9a7
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-11-02 18:29:27 +0200

    Commit

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,9 +1,39 @@
 #include "main.h"
 /**
+ * sqrt_check - Finds the squar root of a number
+ * @n: Number to find sqrt
+ * @i: Natural number to start checking
+ * Return: -1 if not found else sqrt
+ */
+int sqrt_check(int n, int i)
+{
+	if (i * i > n)
+	{
+		return (-1);
+	}
+	else if (i * i == n)
+	{
+		return (i);
+	}
+	else
+	{
+		return (sqrt_check(n, i + 1));
+	}
+}
+
+/**
  * _sqrt_recursion - Functions returns the natural square root of a number
  * @n: Number
  * Return: 1 if n does not have a nutural square root else natural sqrt
  */
 int _sqrt_recursion(int n)
 {
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (sqrt_check(n, 0));
+	}
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,5 +10,6 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int sqrt_check(int n, int i);
 
 #endif /* MAIN_H */
