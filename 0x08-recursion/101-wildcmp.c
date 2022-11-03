6ecb9073e89d6dbbc2e7948307a65371dd5c76ab
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-11-03 15:02:57 +0200

    Commit

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,13 @@
+#include "mainh"
+/**
+ * wildcmp - Function compares strings and returns 1
+ * if the strings can be considered identical, otherwise
+ * return 0.
+ * @s1: String 1
+ * @s2: String 2 can contain special character *
+ * Return: 1 if identical otherwise return 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	int i, int j;
+}
