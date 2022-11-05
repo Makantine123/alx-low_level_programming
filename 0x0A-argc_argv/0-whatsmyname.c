3f29470f97a3f71d3fb855e4275fe9097e99a0b5
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-11-05 07:36:40 +0200

    Commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /**
  * main - print program name
+ * @argc: Number of arguments in cmd line
+ * @argv: Array of arguments in cmd line of size argc
  * Return: Void
  */
 int main(int argc, char *argv[])
