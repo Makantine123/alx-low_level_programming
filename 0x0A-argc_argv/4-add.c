d4d6769f7bda8abdfe7ce5a1c7e913aab536871b
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-11-05 15:15:03 +0200

    Commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,37 +1,89 @@
-#include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
+
 /**
- * main -  Adds 2 positive numbers
- * @argc: Number of arguments
- * @argv: Array of cmd arguments
- * Return: Always 0
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+int _atoi(char *s)
+{
+	int i, d, n, len, f, digit;
+
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+			{
+				digit = -digit;
+			}
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+			{
+				break;
+			}
+			f = 0;
+		}
+		i++;
+	}
+
+	if (f == 0)
+	return (0);
+
+	return (n);
+}
+
+/**
+ * main - adds two positive number
+ * @argc: number of arguments
+ * @argv: array of arguents
+ *
+ * Return: 0 (Success), or 1 (Success)
  */
 int main(int argc, char *argv[])
 {
-	unsigned int i, result;
+	int sum, num, i, j, k;
 
-	result = 0;
+	sum = 0;
 
-	if (argc < 2)
+	for (i = 1; i < argc; i++)
 	{
-		printf("0\n");
-	}
-	else if (argc > 2)
-	{
-		for (i = 1; argv[i] != NULL; i++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (atoi(argv[i]) >= 0 && atoi(argv[i]) <= 10000)
+			if (argv[i][j] > '9' || argv[i][j] < '0')
 			{
-				result = result + atoi(argv[i]);
-			}
-			else
-			{
-				printf("Error\n");
+				puts("Error");
 				return (1);
 			}
 		}
-		printf("%d\n", result);
 	}
+
+	for (k = 1; k < argc; k++)
+	{
+		num = _atoi(argv[k]);
+		if (num >= 0)
+		{
+			sum += num;
+		}
+	}
+
+	printf("%d\n", sum);
 	return (0);
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,6 +2,6 @@
 #define MAIN_H
 
 #include <stdio.h>
-
+int check_digits(char *argv[], int argc);
 
 #endif /* MAIN_H */
