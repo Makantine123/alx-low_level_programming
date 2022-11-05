f72f782ad299bfa11b761fd566924aa32def167e
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-11-05 08:06:44 +0200

    Commit

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Prints all the arguments it recieves
+ * @argc: Number of arguments
+ * @argv: Array of cmd arguments
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
