c70972883320fa2e98e020519f4abbff2cd64b88
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-11-05 07:30:40 +0200

    Commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**main - print program name
+ * Return: Void
+ */
+int main(int argc, char* argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+		return (0);
+	}
+	return (0);
+}
