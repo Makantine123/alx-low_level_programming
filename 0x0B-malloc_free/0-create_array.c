808898dd0fd88c5d668f62a8d7209d9c296e0062
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-11-07 21:44:11 +0200

    Commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - Function creates an array of chars,
+ * and initializes it with a specific char
+ * @size: Size of array
+ * @c: Name of array
+ * Return: Pointer to array or NULL if size = 0
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		s = malloc(sizeof(char) * size);
+
+		while (i < size)
+		{
+			s[i] = c;
+			i++;
+		}
+		s[i] = '\0';
+		return (s);
+	}
+}
+
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+char *create_array(unsigned int size, char c);
+char _putchar(char c);
+
+#endif /* MAIN_H */
