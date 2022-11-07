1f3a2b6409484c2cdd0839f57bd2c5eea3a8ef7b
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-11-07 22:22:08 +0200

    Commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,12 +13,15 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 
 	if (size == 0)
+	{
 		return (NULL);
+	}
 
 	s = (char *) malloc(sizeof(char) * size);
-	
 	if (s == NULL)
+	{
 		return (0);
+	}
 
 	while (i < size)
 	{
