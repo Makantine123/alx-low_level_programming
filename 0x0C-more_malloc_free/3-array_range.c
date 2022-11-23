d5ceac4ccca91ec0df7e0874b8723d982c26c369
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-11-23 14:01:47 +0200

    Commit

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -13,24 +13,24 @@
  */
 int *array_range(int min, int max)
 {
-int *p;
-int size, i, j;
+	int *p;
+	int size, i, j;
 
-if (min > max)
-return (0);
+	if (min > max)
+		return (0);
 
-if (max > min)
-size = max - min + 1;
-else if (max == min)
-size = 2;
+	if (max > min)
+		size = max - min + 1;
+	else if (max == min)
+		size = 2;
 
-p = malloc(sizeof(int) * size);
-if (p == 0)
-return (0);
+	p = malloc(sizeof(int) * size);
+	if (p == 0)
+		return (0);
 
-for (i = 0, j = min; j <= max; i++, j++)
-p[i] = j;
-if (max == min)
-p[i] = max;
-return (p);
+	for (i = 0, j = min; j <= max; i++, j++)
+		p[i] = j;
+		if (max == min)
+		p[i] = max;
+	return (p);
 }
