b67048fa87bca4602ffbe912152375e8ae883466
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-11-23 13:53:19 +0200

    Commit

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,39 +1,36 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- * array_range - Function creates an array of integers
- * @min: Values starting
- * @max: Values ending
- * Return: Pointer to the newly created array
+ * array_range -  creates an array of integers
+ * The array created should contain all the values
+ * from min (included) to max (included), ordered from min to max
+ * @min: minimal value
+ * @max: maximum value
+ *
+ * Return: Pointer to allocated memory of s1 + nbytes of s2
  */
 int *array_range(int min, int max)
 {
-	int size, i, j;
-	int *arr;
+int *p;
+int size, i, j;
 
-	if (min > max)
-	{
-		return (NULL);
-	}
-	for (i = min; i <= max; i++)
+if (min > max)
+return (0);
 
-	size = i + 1;
-	arr = malloc(sizeof(int) * size);
+if (max > min)
+size = max - min + 1;
+else if (max == min)
+size = 2;
 
-	if (arr == 0)
-	{
-		return (NULL);
-	}
+p = malloc(sizeof(int) * size);
+if (p == 0)
+return (0);
 
-	for (i = 0, j = min; j <= max; i++, j++)
-	{
-		arr[i] = j;
-
-		if (min == max)
-		{
-			arr[i] = max;
-		}
-	}
-	return (arr);
+for (i = 0, j = min; j <= max; i++, j++)
+p[i] = j;
+if (max == min)
+p[i] = max;
+return (p);
 }
