581d2c3b521f035369a8ea36347ed6df1483c262
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-12-01 21:38:23 +0200

    Commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+/**
+ * print_name - Prints name
+ * @name: Name 
+ * @f:pointer to the printing function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+	{
+		return;
+	}
+	f(name);
+}
