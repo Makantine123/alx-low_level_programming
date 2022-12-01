c58f7b4d7b8c4d92a1e159e5bd740ace6a9edb04
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-12-01 18:31:43 +0200

    Commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A dogs basic info
+ * @name: Dogs name
+ * @age: Dogs age
+ * @owner: Dog's owner
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	char *age;
+	char *owner;
+};
