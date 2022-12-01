3271c051a1668a5b00dec911022cea9833f67c56
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-12-01 19:28:47 +0200

    Commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -19,7 +19,7 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: %s\n", d->name);
 	}
-	printf("Age: %f\n",d->age);
+	printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
 	{
 		printf("Owner: (nil)\n");
