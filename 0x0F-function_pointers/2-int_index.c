202a555d3795be3ea88796de5ef45d46794392bb
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-12-02 13:03:55 +0200

    Commit

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+/**
+ * int_index - Function searches for an interger
+ * @array: Array
+ * @size: Number of elements
+ * @cmp: function pointer
+ * Return: Always index of match, -1 if no matches or size < 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size < 0)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+
+	return (-1);
+}
