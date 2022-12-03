90674208594c294ec5de5bd4db7f7be47872abe2
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-12-03 13:08:59 +0200

    Commit

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -13,41 +13,41 @@ void print_all(const char * const format, ...)
 
 	i = 0;
 
-	va_start(par, format);
-
 	if (format)
 	{
-	while (format[i])
-	{
-		switch (format[i])
+		va_start(par, format);
+		while (format[i])
 		{
-			case 'c':
-				printf("%c", va_arg(par, int));
-				break;
-			case 'i':
-				printf("%d", va_arg(par, int));
-				break;
-			case 'f':
-				printf("%f", va_arg(par, double));
-				break;
-			case 's':
-				str = va_arg(par, char *);
-				if (str == NULL)
-				{
-					str = "(nil)";
-				}
-				printf("%s", str);
-				break;
-			default:
-				i++;
-				continue;
+			switch (format[i])
+			{
+				case 'c':
+					printf("%c", va_arg(par, int));
+					break;
+				case 'i':
+					printf("%d", va_arg(par, int));
+					break;
+				case 'f':
+					printf("%f", va_arg(par, double));
+					break;
+				case 's':
+					str = va_arg(par, char *);
+					if (str == NULL)
+					{
+						str = "(nil)";
+					}
+					printf("%s", str);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			if (format[i + 1] != '\0')
+			{
+				printf(", ");
+			}
+			i++;
 		}
-		if (format[i + 1] != '\0')
-		{
-			printf(", ");
-		}
-		i++;
-		}
+		va_end(par);
 	}
-	printf("\n"), va_end(par);
+	printf("\n");
 }
