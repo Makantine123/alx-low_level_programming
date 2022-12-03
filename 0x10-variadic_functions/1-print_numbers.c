01eaf37099c022f3d187e30bd82c69643cf044e4
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-12-03 03:54:57 +0200

    Commit

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,7 +12,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(par, n);
 
-	for (i = 1; i < n; i++)
+	for (i = 1; i <= n; i++)
 	{
 		printf("%d", va_arg(par, int));
 
