82b8664eb2201a1d640e8dce5b3eef8cdc283f7e
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-12-03 03:13:43 +0200

    Commit

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+/*
+ * sum_them_all - Sums all of its parameters
+ * @n: Parameters
+ *
+ * Return: Sum of the parameters, 0 if n == 0
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list par;
+	unsigned int i;
+	int sum;
+
+	va_start (par, n);
+
+	sum = 0;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg (par, int);
+	}
+	
+	va_end (par);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef VARIADIC_FUNC_H
+#define VARIADIC_FUNC_H
+
+#include "stdarg.h"
+#include <stdio.h>
+
+int sum_them_all(const unsigned int n, ...);
+
+
+#endif
