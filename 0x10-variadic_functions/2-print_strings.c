0e3aa97f69876a8af1447a0090c4844c630c74f3
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-12-03 04:25:11 +0200

    Commit

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -7,15 +7,15 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-        va_list par;
+	va_list par;
         unsigned int i;
 
         va_start(par, n);
 
-        for (i = 0; i <= n; i++)
+        for (i = 1; i <= n; i++)
         {
                 printf("%s", va_arg(par, char *));
-		
+
                 if ((i <= n - 1) && (separator != NULL))
                 {
                         printf("%s", separator);
