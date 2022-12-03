bffde79553d5ebe541d94675d87334506b8aee9e
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-12-03 12:51:38 +0200

    Commit

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,51 @@
+#include "variadic_functions.h"
+/**
+ * print_all - Functions that prints anything
+ * @format: List of types of arguments passed to the function
+ * Description: If the string is NULL, print (nil), else print
+ * Return: Nothing
+ */
+void print_all(const char * const format, ...)
+{
+	va_list par;
+	int i;
+	char *str;
+
+	i = 0;
+
+	va_start(par, format);
+
+	while (format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(par, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(par, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(par, double));
+				break;
+			case 's':
+				str = va_arg(par, char *);
+				if (str == NULL)
+				{
+					str = "(nil)";
+				}
+				printf("%s", str);
+				break;
+			default:
+				i++;
+				continue;
+		}
+		if (format[i + 1] != '\0')
+		{
+			printf(", ");
+		}
+		i++;
+		}
+		printf("\n");
+		va_end(par);
+		}
