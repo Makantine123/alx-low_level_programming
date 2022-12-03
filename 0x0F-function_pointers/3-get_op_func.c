81a0e610feb653dbef13e10a0f2b60175a08b603
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-12-03 02:05:17 +0200

    Commit

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -7,14 +7,19 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-	op_t ops[] = {{"+", op_add}, {"-", op_sub}, {"*", op_mul}, {"/", op_div}, {"%", op_mod}, {NULL, NULL}};
+	op_t ops[] = {{"+", op_add}, 
+		{"-", op_sub}, 
+		{"*", op_mul}, 
+		{"/", op_div}, 
+		{"%", op_mod}, 
+		{NULL, NULL}};
 	int i;
 
 	i = 0;
 
 	while (ops[i].op)
 	{
-		if (s == ops[i].op)
+		if (strcmp(ops[i].op, s) == 0)
 		{
 			return (ops[i].f);
 		}
