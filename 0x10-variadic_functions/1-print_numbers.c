9e354a1e874cb69f50c3ba5de808ba876489d203
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-12-03 03:47:48 +0200

    Commit

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - Prints numbers followed by new line
+ * @separator: String to be printed with number
+ * @n: Numbers
+ * Return: Nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list par;
+	unsigned int i;
+
+	va_start(par, n);
+
+	for (i = 1; i < n; i++)
+	{
+		printf("%d", va_arg(par, int));
+
+		if (i < n -1)
+		{
+			printf(" %s ", separator);
+		}
+	}
+	printf("\n");
+}
