00db6badfc318b746e87230de86e1e705d15c899
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-12-03 04:14:55 +0200

    Commit

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - Prints strings followed by a new line
+ * @separator: String to be printed between the strings
+ * @n: Number of strings passed to the function
+ * Return: Nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+        va_list par;
+        unsigned int i;
+
+        va_start(par, n);
+
+        for (i = 1; i <= n; i++)
+        {
+                printf("%s", va_arg(par, char *));
+
+                if ((i <= n - 1) && (separator != NULL))
+                {
+                        printf("%s", separator);
+                }
+        }
+        printf("\n");
+}
+
