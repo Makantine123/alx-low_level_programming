7e958a073da8e017f806236c396aba9ce2d3e752
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-12-05 11:11:33 +0200

    Commit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,12 +7,12 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-        if (index > 63)
-        {
-                return (-1);
-        }
+	if (index > 63)
+	{
+		return (-1);
+	}
 
-        *n = ((1UL << index) | *n);
+	*n = ((1UL << index) | *n);
 
-        return (1);
+	return (1);
 }
