9869293b6cb284070ca7ea00c30b47a8a06dbe4b
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-12-06 13:59:01 +0200

    Commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,26 +7,26 @@
  */
 size_t print_list(const list_t *h)
 {
-int i;
+	int i;
 
-if (h)
-{
-for (i = 0; h; i++)
-{
-if (h->str == NULL)
-{
-printf("[0] (nil)\n");
-}
-else
-{
-printf("[%u] %s\n", h->len, h->str);
-}
-h = h->next;
-}
-return (i);
-}
-else
-{
-return (0);
-}
+	if (h)
+	{
+		for (i = 0; h; i++)
+		{
+			if (h->str == NULL)
+			{
+				printf("[0] (nil)\n");
+			}
+			else
+			{
+				printf("[%u] %s\n", h->len, h->str);
+			}
+			h = h->next;
+		}
+		return (i);
+		}
+	else
+	{
+		return (0);
+	}
 }
