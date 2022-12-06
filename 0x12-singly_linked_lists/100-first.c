523dc29b042f9f1e82c2a355ad436a338bf05889
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-12-06 19:14:31 +0200

    Commit

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * race - Function prints a string
+ * Return: Nothing
+ */
+void race(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
