5558a22e761044cfa840153888f124aa5b22fc8b
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2022-12-07 17:18:55 +0200

    Commit

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *temp = *head;
 	unsigned int count = 0;
 
-	if (!head || !*head)
+	if (*head == NULL)
 	{
 		return (-1);
 	}
