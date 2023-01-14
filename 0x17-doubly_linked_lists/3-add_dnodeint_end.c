b40d98369e41b1f883b0cdec52fd59d645278640
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-01-14 16:19:52 +0200

    Commit

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -24,10 +24,11 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
         new_node->next = NULL;
     }
         if (*head == NULL)
+        {
             new_node->prev = NULL;
             *head = new_node;
             return (new_node);
-
+        }
         while (last_node->next != NULL)
             last_node = last_node->next;
 
