395c2d76c833dc51a5e4a5579417dc56c4850e5e
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-01-14 16:36:46 +0200

    Commit

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * free_dlistint - Function frees a dlistint_t
+ * @head: List head
+ *
+ * Return: Nothing
+ */
+void free_dlistint(dlistint_t *head)
+{
+    dlistint_t *current, *next;
+
+    current = head;
+
+    while (current != NULL)
+    {
+        next = current->next;
+        free(current);
+        current = next;
+    }
+
+    *head = NULL;
+}
