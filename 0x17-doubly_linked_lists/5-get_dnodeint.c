772fb5061330bc28b0981fe9cb77335c90003c47
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-01-14 21:43:06 +0200

    Commit

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -8,14 +8,17 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
+dlistint_t *current = head;
 unsigned int count = 0;
 
-while (head != NULL)
+while (current != NULL)
 {
 if (count == index)
 {
-return ((*head)->n);
+return (current->n);
 }
+count++;
+curent = current->next;
 }
 return (NULL);
 }
