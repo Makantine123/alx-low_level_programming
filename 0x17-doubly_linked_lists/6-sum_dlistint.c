176e2c85d53c19e5ce0663a4ba03515c9d1ccf62
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-01-14 22:03:36 +0200

    Commit

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * sum_dlistint - Function returns the sum of all data (n) of list
+ * @head: list head
+ *
+ * Return: 0 if list is empty, else sum of all data (n)
+ */
+int sum_dlistint(dlistint_t *head)
+{
+    dlistint_t *current = head;
+    int sum = 0;
+
+    if (!curent)
+    {
+        return (0);
+    }
+    while (current != NULL)
+    {
+        sum += current->n;
+        current = current->next;
+    }
+    return (sum)
+}
