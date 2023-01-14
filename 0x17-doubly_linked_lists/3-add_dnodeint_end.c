59202f4cb9b7003ab61300ce1ba83c8c658ce833
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-01-14 16:21:39 +0200

    Commit

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -8,31 +8,31 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node;
-    dlistint_t *last_node;
+dlistint_t *new_node;
+dlistint_t *last_node;
 
-    if (!head)
-        return (NULL);
+if (!head)
+return (NULL);
 
-    new_node = malloc(sizeof(dlistint_t)); 
-    if (!new_node)
-        return (NULL);
-    else
-    {
-        last_node = *head;
-        new_node->n = n;
-        new_node->next = NULL;
-    }
-        if (*head == NULL)
-        {
-            new_node->prev = NULL;
-            *head = new_node;
-            return (new_node);
-        }
-        while (last_node->next != NULL)
-            last_node = last_node->next;
+new_node = malloc(sizeof(dlistint_t)); 
+if (!new_node)
+return (NULL);
+else
+{
+last_node = *head;
+new_node->n = n;
+new_node->next = NULL;
+}
+if (*head == NULL)
+{
+new_node->prev = NULL;
+*head = new_node;
+return (new_node);
+}
+while (last_node->next != NULL)
+last_node = last_node->next;
 
-        last_node->next = new_node;
-        new_node->prev = last_node;
-        return (new_node);
+last_node->next = new_node;
+new_node->prev = last_node;
+return (new_node);
 }
