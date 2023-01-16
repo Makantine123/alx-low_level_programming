796942e72ae99b342ecbb6f98efd71e20729c8bc
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-01-16 08:17:46 +0200

    Commit

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,46 +1,45 @@
 #include "lists.h"
+
 /**
- * insert_dnodeint_at_index - Inserts new node at a given position
- * @h: list head
- * @idx: node index 
- * @n: Data
+ * insert_dnodeint_at_index - inserts dlistint node at index
  *
- * Return: Adress to the node or NULL
+ * @h: head of list
+ * @idx: index of node
+ * @n: value for new node
+ *
+ * Return: new node address, or NULL if index not accessible or malloc fail
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-dlistint_t *new_node, *tmpcount = *h, *tmp1 = *h, *tmp2;
-unsigned int count = 0;
+	dlistint_t *ptr = *h, *new;
 
-if (!h)
-return (NULL);
-while (tmpcount)
-tmpcount = tmpcount->next, count++;
-if (idx > count)
-return (NULL);
-
-new_node = malloc(sizeof(dlistint_t));
-if (!new_node)
-return (NULL);
-
-new_node->n = n;
-if (idx == 0)
-{
-new_node->prev = NULL, new_node->next = *h;
-if (*h)
-(*h)->prev = new_node;
-*h = new_node;
-return (new_node);
-}
-
-for (count = 0; count < idx; count++)
-tmp2 = tmp1, tmp1 = tmp1->next;
-
-new_node->next = tmp1;
-new_node->prev = tmp2;
-tmp2->next = new_node;
-if (tmp1)
-tmp1->prev = new_node;
-
-return (new_node);
+	if (idx == 0)
+	{
+		new = malloc(sizeof(dlistint_t));
+		if (new == NULL)
+			return (NULL);
+		new->n = n;
+		new->next = *h;
+		if (*h != NULL)
+			(*h)->prev = new;
+		new->prev = NULL;
+		*h = new;
+		return (new);
+	}
+	if (ptr == NULL)
+		return (NULL);
+	while (ptr->next != NULL && idx-- > 1)
+		ptr = ptr->next;
+	if (idx > 1)
+		return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = ptr->next;
+	new->prev = ptr;
+	ptr->next = new;
+	if (new->next != NULL)
+		new->next->prev = new;
+	return (new);
 }
