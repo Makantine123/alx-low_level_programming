4312d75f777bb907a309c7f5861f5c7f9e492ac7
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-01-18 11:59:07 +0200

    Commit

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Makantine123/alx-low_level_programming/0x18-dynamic_libraries/hijackgame.so
+wget -qO /tmp/jackpot.so https://github.com/Makantine123/alx-low_level_programming/raw/master/0x18-dynamic_libraries/hijackgame.so
 export LD_PRELOAD=/tmp/hijackgame.so
