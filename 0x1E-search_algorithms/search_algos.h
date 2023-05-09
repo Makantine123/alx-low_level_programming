5945b9a911e58449f270e261a56938a57c17f24b
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-05-09 11:30:58 +0200

    Commit

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,12 +1,9 @@
-#ifndef SEARCH_ALGOS
-#define SEARCH_ALGOS
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
 
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
-* Prototypes
-*/
 int linear_search (int *array, size_t size, int value);
 
 #endif
