ed7bdc2127ad47e9aeb1c17ae71a0625f3eb9a90
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-05-09 22:42:03 +0200

    Commit

---
+++ 0x1E-search_algorithms/103-exponential.c
@@ -0,0 +1,50 @@
+#include "search_algos.h"
+/**
+ * exponential_search - Searches for a value in a sorted array of integers
+ *                      using the Exponential search algorithm
+ * @array: A pointer to the first element of the array to search in
+ * @size: The number of elements in the array
+ * @value: The value to search for
+ *
+ * Return: The first index where value is located, or -1 if not found
+ */
+int exponential_search(int *array, size_t size, int value)
+{
+	int bound = 1;
+	int low, high;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	while (bound < (int)size && array[bound] < value)
+	{
+		printf("Value checked array[%d] = [%d]\n", bound, array[bound]);
+		bound *= 2;
+	}
+
+	low = bound / 2;
+	high = (bound < (int)size) ? bound : (int)size - 1;
+
+	while (low <= high)
+	{
+		int mid = (low + high) / 2;
+
+		printf("Searching in array: ");
+		for (int i = low; i <= high; i++)
+		{
+			printf("%d", array[i]);
+			if (i < high)
+				printf(", ");
+		}
+		printf("\n");
+
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			low = mid + 1;
+		else
+			high = mid - 1;
+	}
+
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,7 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
 
+#include <cstddef>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,5 +11,6 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
 
 #endif
